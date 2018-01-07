0f7f13ed4330018f0e2e7f9bc1097f0273168c6b
OpalCadman <little_opal@hotmail.co.uk>
2018-01-07 14:11:48 +0000

    Fixed Code and issue with score manager. Added new animations more to do

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -23,49 +23,20 @@ public class GunScript : MonoBehaviour {
     public float shotspershot = 1;
     public Text textbox;
 	GameObject pewpew;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     string playertag; // new
-=======
     public Animator animator;
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
-
->>>>>>> 349ed7a4731348feb38ce03d1e07ddbcaa87dfab
 
     // Use this for initialization
     void Start ()
     {
         baseammo = ammo;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
         Renderer rend = GetComponent<Renderer>();
         rend.material = new Material(gunShader);
         rend.material.mainTexture = gunText;
         rend.material.color = gunColor;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         if (this.transform.parent.parent) { playertag = this.transform.parent.parent.tag; } //new need to assign player tags to p1,2,3,4
-=======
->>>>>>> 349ed7a4731348feb38ce03d1e07ddbcaa87dfab
-=======
-        
-
->>>>>>> parent of 1b9d7ce... Started to add animations
-=======
-        
-
->>>>>>> parent of 1b9d7ce... Started to add animations
     }
 	
 	// Update is called once per frame
@@ -83,14 +54,8 @@ public class GunScript : MonoBehaviour {
             else
             {
                 fire();
-<<<<<<< HEAD
-<<<<<<< HEAD
                 animator.SetBool("IsShooting", true);
                 reloadcheck = true;
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
 
             }
         }
@@ -116,28 +81,7 @@ public class GunScript : MonoBehaviour {
         if (Time.time > firerate + lastShot)
         {
             ammo -= shotspershot;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
             pewpew = (GameObject)Instantiate(bulletPrefab, transform.GetChild(0).transform.position, Quaternion.identity);
-<<<<<<< HEAD
-=======
-            Ray ray = transform.parent.GetChild(0).GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                pewpew.transform.LookAt(hit.point);
-                pewpew.GetComponent<Rigidbody>().velocity = pewpew.transform.forward * -bulletspeed;
-            }
-            else
-            {
-                pewpew.GetComponent<Rigidbody>().velocity = ray.direction * -bulletspeed;
-            }
->>>>>>> 349ed7a4731348feb38ce03d1e07ddbcaa87dfab
-=======
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
-			pewpew = (GameObject)Instantiate (bulletPrefab, transform.GetChild (0).transform.position, Quaternion.identity);
 			Ray ray = transform.parent.GetChild (0).GetComponent<Camera> ().ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0));
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit)) {
@@ -146,24 +90,8 @@ public class GunScript : MonoBehaviour {
 			} else {
 				pewpew.GetComponent<Rigidbody> ().velocity = ray.direction * -bulletspeed;
 			}
-<<<<<<< HEAD
->>>>>>> parent of 1b9d7ce... Started to add animations
 
             pewpew.GetComponent<KillBullet>().Setfiretag(playertag);
-=======
->>>>>>> parent of 1b9d7ce... Started to add animations
-
-            Ray ray = transform.parent.GetChild(0).GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                pewpew.transform.LookAt(hit.point);
-                pewpew.GetComponent<Rigidbody>().velocity = pewpew.transform.forward * -bulletspeed;
-            }
-            else
-            {
-                pewpew.GetComponent<Rigidbody>().velocity = ray.direction * -bulletspeed;
-            }
 
             lastShot = Time.time;
         }
