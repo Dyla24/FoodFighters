ad87243c75486510880c21f056960a19b847d642
OpalCadman <little_opal@hotmail.co.uk>
2018-01-08 01:14:57 +0000

    Adding animation

---
+++ Assets/Scripts/PowerUpSpawns.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawns : MonoBehaviour {
+
+    public List<GameObject> powerUps = new List<GameObject>();
+    public List<GameObject> PU_spawnPoints = new List<GameObject>();
+    private List<GameObject> PU_active = new List<GameObject>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        spawnObjects();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void spawnObjects()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            PU_active.Add(Instantiate(powerUps[i], PU_spawnPoints[i].transform.position, PU_spawnPoints[i].transform.rotation));
+        }
+    }
+}
