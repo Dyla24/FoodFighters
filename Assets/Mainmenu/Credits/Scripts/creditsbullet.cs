2c405c938be3af56159e48bda910b4a3de555783
ELL1010 <elliottbarrett25@gmail.com>
2018-01-10 20:38:48 +0000

    sound effects added to credits

--- Assets/Mainmenu/Credits/Scripts/creditsbullet.cs
+++ Assets/Mainmenu/Credits/Scripts/creditsbullet.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class creditsbullet : MonoBehaviour {
 	public GameObject splat;
+	public AudioClip crash;
+	public AudioClip splats;
+	public AudioSource audios;
 	
 
 	// Use this for initialization
 	void Start () {
-		
+		audios = GameObject.FindGameObjectWithTag ("Canvas").GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -23,9 +26,15 @@ public class creditsbullet : MonoBehaviour {
 			GameObject canvas = GameObject.FindGameObjectWithTag ("Canvas");
 			GameObject s = (GameObject)Instantiate (splat, transform.position, Quaternion.identity, canvas.transform.GetChild (0));
 			s.GetComponent<Image> ().color = Random.ColorHSV (0f,1f,0f,1f,0.5f,1f,1,1);
+			audios.clip = splats;
+			audios.volume = 1;
+			audios.Play ();
 		} else {
 			Destroy (this.gameObject);
 			other.transform.parent.GetComponent<Containerscript> ().explosion (transform.position);
+			audios.clip = crash;
+			audios.volume = 0.5;
+			audios.Play ();
 		}
 	}
 }
